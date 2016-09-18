8e4863a6e5e5968c7d914017c4e5584028b1a2e4
asmsuechan <suenagaryoutaabc@gmail.com>
2016-09-18 23:54:01 +0900

    インストールスクリプトを更新

--- env_setup/setup_mac.sh
+++ env_setup/setup_mac.sh
@@ -10,6 +10,7 @@ fi
 xcode-select --install
 brew install git
 brew tap caskroom/cask
+brew tap codekitchen/dinghy
 
 # brew caskからアプリケーションのインストール
 # cask外からインストールして既存の場合は上書き禁止にしたい
@@ -18,11 +19,27 @@ brew cask install flux
 brew cask install iterm2
 brew cask install vagrant
 brew cask install slack
+brew cask install skype
+brew cask install wireshark
+brew cask install evernote
 
 if [[ -f /Application/Google\ Chrome.app ]];then
   echo "starting install google-chrome"
   brew cask install google-chrome
   echo "install finished!"
+fi
+
+# Boostnoteのインストール。.dmgをマウントしたらBoostnote.appがいる
+# ここ参照
+# https://hnakamur.github.io/blog/2015/04/06/install-apps-without-homebrew-cask/
+if [[ -f /Application/Boostnote.app ]];then
+  echo "starting install Boostnote"
+  wget https://github.com/BoostIO/boost-releases/releases/download/v0.6.5/Boostnote-mac.dmg
+  mount_dir=`hdiutil attach $dmg_file | awk -F '\t' 'END{print $NF}'`
+  cp $mount_dir/Boostnote.app /Application
+  hdiutil detach "$mount_dir"
+  rm $dmg_file
+  echo "install finished"
 fi
 
 # zshのインストール、設定
@@ -41,11 +58,7 @@ elif [ ! -f ~/.zshrc ];then
   curl https://raw.githubusercontent.com/asmsuechan/dotfiles/master/.zshrc > ~/.zshrc
 fi
 
-echo 'autoload -Uz compinit
-compinit' >> ~/.zshenv
-
-# ログインシェルをzshに変更
-sudo dscl . -create /Users/$USER UserShell `which zsh`
+echo 'autoload -Uz compinit' >> ~/.zshenv
 
 # vimのインストール
 if [ -f /usr/bin/vim ];then
@@ -65,22 +78,26 @@ if [ ! -f ~/.vimrc ];then
 fi
 
 # 便利ツール達のインストール
-# elcapitanにmysql5.7インストールすると「「「ヤバい」」」が構わず5.7(edge)をインストールする
+# elcapitanにmysql5.7インストールすると「「「ヤバい」」」が構わず5.7をインストールする
 brew install tig
 brew install rbenv
 brew install mysql
 brew install tree
 brew install docker
 brew install docker-machine
+brew install dinghy
 brew install nginx
 
 # rbenvを使ってrubyのインストール
-rbenv install 2.2.3
-rbenv global 2.2.3
+rbenv install 2.3.1
+rbenv global 2.3.1
 rbenv rehash
 
-sudo gem install rails
+gem install rails
 
 # 最後にインストールチェック
 # ruby rails virtualbox iterm2 zsh
 # あとで書く
+
+# ログインシェルをzshに変更
+sudo dscl . -create /Users/$USER UserShell `which zsh`
