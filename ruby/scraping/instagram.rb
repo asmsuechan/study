f594f2e4403247cf01fe2426afcff15d5361eab3
asmsuechan <suenagaryoutaabc@gmail.com>
2016-09-18 23:18:52 +0900

    変数名の簡略化、クラス名の変更

--- ruby/scraping/instagram.rb
+++ ruby/scraping/instagram.rb
@@ -2,8 +2,7 @@
 require 'mechanize'
 require 'json'
 
-# クラス名変える
-class Instagram
+class ScrapingInstagram
   BASE_URL = 'https://www.instagram.com'
 
   # instagramはcurl叩いたらわかるようにhtmlのbodyが返ってこずにjsonが返ってくる
@@ -13,23 +12,22 @@ class Instagram
   # 以下のような形式のurlが含まれるarrayが返ってくる
   # "https://scontent-nrt1-1.cdninstagram.com/t51.2885-15/e35/14240825_175988942810352_2132301197_n.jpg?ig_cache_key=MTM0MDU5NDUwNTg3NDgzMDc0NQ%3D%3D.2"
   def self.picture_urls_from_tag(tag)
-    agent = Mechanize.new
-    instagram_page_response = agent.get("#{BASE_URL}/explore/tags/#{tag}/")
-                              .search('script').inner_text
-                              .slice(/window\._sharedData\ =(.*)$/)
-                              .gsub("window._sharedData\ = ", '')
-                              .gsub(';', '')
+    page_response = Mechanize.new.get("#{BASE_URL}/explore/tags/#{tag}/")
+                    .search('script').inner_text
+                    .slice(/window\._sharedData\ =(.*)$/)
+                    .gsub("window._sharedData\ = ", '')
+                    .gsub(';', '')
 
+    tag_contents = JSON.parse(page_response)["entry_data"]["TagPage"]
     # puts instagram_tag_content.length
     # => 1
     # パラメーターなしでただtagをurlで指定しただけだと1ページしか表示されない
-    instagram_tag_content = JSON.parse(instagram_page_response)["entry_data"]["TagPage"]
 
-    instagram_tag_content.first["tag"]["media"]["nodes"]
+    tag_contents.first["tag"]["media"]["nodes"]
     .map { |post| post["display_src"] }
-    # パラメーターいらなかったら以下のコメントを外してください
+    # ?以降のパラメーターいらなかったら以下のコメントを外してください
     #.map { |url| url.gsub(/\?(.*)$/, '') }
   end
 end
 
-puts Instagram.picture_urls_from_tag('udon')
+puts ScrapingInstagram.picture_urls_from_tag('udon')
