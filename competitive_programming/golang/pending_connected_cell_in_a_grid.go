8a4614c6839dc68ba561a87c0267d08f027cf310
asmsuechan <suenagaryoutaabc@gmail.com>
2016-09-24 14:02:48 +0900

    Pending Connected Cells in a Grid
    https://www.hackerrank.com/challenges/connected-cell-in-a-grid

---
+++ competitive_programming/golang/pending_connected_cell_in_a_grid.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	n, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return n
+}
+
+func scanArray(n int) []int {
+	ar := []int{}
+	for i := 0; i < n; i++ {
+		ar = append(ar, nextInt())
+	}
+	return ar
+}
+
+func printArray(a []int) {
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("%d ", a[i])
+	}
+	fmt.Println("")
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n := nextInt()
+	m := nextInt()
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = append(matrix[i], scanArray(m)...)
+	}
+	numberOfFilledCells := 0
+	fmt.Println(matrix)
+	fmt.Println(numberOfFilledCells)
+}
